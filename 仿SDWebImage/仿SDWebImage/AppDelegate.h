3ec0438df727347ed17327c9e84f9e958da7e794
zhangxiaochuZXC <zhangjiesharp@163.com>
2016-08-20 17:16:04 +0800

    创建了项目

---
+++ 仿SDWebImage/仿SDWebImage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  仿SDWebImage
+//
+//  Created by HM on 16/8/20.
+//  Copyright © 2016年 HM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 仿SDWebImage/仿SDWebImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  仿SDWebImage
+//
+//  Created by HM on 16/8/20.
+//  Copyright © 2016年 HM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
