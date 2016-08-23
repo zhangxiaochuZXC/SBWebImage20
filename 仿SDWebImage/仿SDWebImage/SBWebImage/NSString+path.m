7c0b31ba6d2ea5d3fd9d1ccb4a2f0da34b13c196
zhangxiaochuZXC <zhangjiesharp@163.com>
2016-08-23 10:41:49 +0800

    MD5编码图片地址实现沙盒缓存

--- 仿SDWebImage/仿SDWebImage/SBWebImage/NSString+path.m
+++ 仿SDWebImage/仿SDWebImage/SBWebImage/NSString+path.m
@@ -7,6 +7,7 @@
 //
 
 #import "NSString+path.h"
+#import "NSString+Hash.h"
 
 @implementation NSString (path)
 
@@ -20,7 +21,10 @@
     // 获取文件名 : http://p16.qhimg.com/dr/48_48_/t0125e8d438ae9d2fbb.png
     // self : 这个方法的调用者
     // lastPathComponent : 截取网络地址最后一个`/`后面的内容(就是图片名)
-    NSString *name = [self lastPathComponent];
+//    NSString *name = [self lastPathComponent];
+    
+    // 需要把图片整个地址进行MD5编码,生成的32位的字符串作为图片缓存的名字
+    NSString *name = [self md5String];
     
     // 路径拼接文件名
     // stringByAppendingPathComponent : 会自动添加`/`
